bd2aac8dba02f4df9b83ff707dd92e4b00c317d9
Danya <theincredibledanzel@yandex.ru>
2022-06-13 23:30:23 +0400

    init commit

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿int number = new Random().Next(10, 100);
+int max = int.MinValue;
+
+Console.WriteLine(number);
+
+while(number > 0)
+{
+    int lastDigit = number % 10;
+    if(lastDigit > max)
+    {
+        max = lastDigit;
+    }
+    number = number / 10;
+}
+
+Console.WriteLine(max);
