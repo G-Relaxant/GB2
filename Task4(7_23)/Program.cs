191bc9b9d9ed1ca47d926966ad1818cc56363a76
geekbrains <info@geekbrains.ru>
2022-06-14 19:35:57 +0400

    edit

--- Task4(7_23)/Program.cs
+++ Task4(7_23)/Program.cs
@@ -22,7 +22,7 @@ else
 
 
 //VARIANT 2
-Console.Write("Введите число: ");
+/*Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int divrem7 = number % 7;
 int divrem23 = number % 23;
@@ -33,4 +33,24 @@ if(divrem7 == 0 && divrem23 == 0)
 else
 {
     Console.WriteLine("No, undivisible");
+}*/
+
+
+
+// Variant 3
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(CheckMultiple(number, 7) && CheckMultiple(number, 23))    // Применяем метод сравнения(53 строка)
+{
+    Console.WriteLine("Yes, divisible");
+}
+else
+{
+    Console.WriteLine("No, undivisible");
+}
+
+bool CheckMultiple(int a, int b)                     // Инициализируем метод сравнения
+{
+    return a % b == 0;
 }
