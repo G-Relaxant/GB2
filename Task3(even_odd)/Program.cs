dddc65ebc8b35c462b5b2f08a06a6f41aabc9003
geekbrains <info@geekbrains.ru>
2022-06-14 18:36:11 +0400

    task3_and_task4

---
+++ Task3(even_odd)/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int remnant = number1 % number2;
+if(remnant == 0)
+{
+    Console.WriteLine("Первое число кратно второму");
+}
+else
+{
+    Console.WriteLine("Первое число не кратно второму. Остаток = " + remnant);
+}
---
+++ Task4(7_23)/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int divrem7 = number1 % 7;
+int divrem23 = number1 % 23;
+if(divrem7 == 0)
+{
+    if(divrem23 == 0)
+    {
+        Console.WriteLine("Да, кратно");
+    }
+    else
+    {
+        Console.WriteLine("Нет, не кратно");
+    }
+}
+else
+    {
+        Console.WriteLine("Нет, не кратно");
+    }
